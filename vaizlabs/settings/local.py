13150acae10a6dc1278df0b04e7e1b524848f718
Arun Vaidya <arun.vaidya@live.com>
2017-07-11 07:35:36 +0530

    Moving to production level settings configuration for django

---
+++ vaizlabs/settings/local.py
@@ -0,0 +1,8 @@
+
+from base import *
+
+DEBUG = True
+
+# Enable security for website
+# Force HTTPS
+SECURE_SSL_REDIRECT = False # [1]
---
+++ vaizlabs/settings/production.py
@@ -0,0 +1,8 @@
+
+from base import *
+
+DEBUG = False
+
+# Enable security for website
+# Force HTTPS
+SECURE_SSL_REDIRECT = True # [1]
